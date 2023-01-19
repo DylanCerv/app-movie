d34f83223c12d36ab6e49da990ec2331b09bea49
DYlan <gownerbeats@gmail.com>
2023-01-18 19:07:13 -0500

    add links footer

--- src/Components/Layout/Footer/Footer.jsx
+++ src/Components/Layout/Footer/Footer.jsx
@@ -5,9 +5,9 @@ export default function Footer() {
     <footer className='bottom-0 py-5 px-40 w-full flex flex-row justify-center gap-10 bg-black md:justify-between'>
       <img className='w-20' src="/src/assets/svg/logo.svg" alt="" />
       <div className='flex flex-col gap-2'>
-        <a href="">Linkedin</a>
-        <a href="">Github</a>
-        <a href="">Portafolio</a>
+        <a href="https://www.linkedin.com/in/dylan-espana/" target={'_blank'}>Linkedin</a>
+        <a href="https://github.com/DylanCerv" target={'_blank'}>Github</a>
+        <a href="https://dylan-cerv.web.app/" target={'_blank'}>Portafolio</a>
       </div>
     </footer>
   )
